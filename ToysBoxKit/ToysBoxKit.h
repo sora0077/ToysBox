1e371aad7123829560f5501dc885d61ef8c4f6db
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-06-06 22:15:58 +0900

    Initial Commit

---
+++ ToysBoxKit/ToysBoxKit.h
@@ -0,0 +1,19 @@
+//
+//  ToysBoxKit.h
+//  ToysBoxKit
+//
+//  Created by 林達也 on 2015/06/06.
+//  Copyright (c) 2015年 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToysBoxKit.
+FOUNDATION_EXPORT double ToysBoxKitVersionNumber;
+
+//! Project version string for ToysBoxKit.
+FOUNDATION_EXPORT const unsigned char ToysBoxKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToysBoxKit/PublicHeader.h>
+
+
